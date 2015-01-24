811ba90f30fbee2c4573328b23ae0861bc7b508f
Luca Realdi <luca.realdi@opencontent.it>
2015-01-24 19:42:38 +0100

    ini prototipo

--- doc/prototipo_sensor/override.ini.append.php
+++ doc/prototipo_sensor/override.ini.append.php
@@ -2,15 +2,38 @@
 
 [posts]
 Source=node/view/full.tpl
-MatchFile=sensor/posts.tpl
+MatchFile=post/posts.tpl
 Subdir=templates
 Match[class_identifier]=folder
 Match[section_identifier]=sensor
 
-[full_folder]
+[dimmi_root]
 Source=node/view/full.tpl
-MatchFile=full/folder.tpl
+MatchFile=forum/root.tpl
 Subdir=templates
-Match[class_identifier]=folder
+Match[class_identifier]=dimmi_root
+Match[section_identifier]=sensor
+
+[dimmi_forum]
+Source=node/view/full.tpl
+MatchFile=forum/forum.tpl
+Subdir=templates
+Match[class_identifier]=dimmi_forum
+Match[section_identifier]=sensor
+
+[dimmi_forum_topic]
+Source=node/view/full.tpl
+MatchFile=forum/forum_topic.tpl
+Subdir=templates
+Match[class_identifier]=dimmi_forum_topic
+Match[section_identifier]=sensor
+
+[dimmi_forum_reply]
+Source=node/view/full.tpl
+MatchFile=forum/forum_reply.tpl
+Subdir=templates
+Match[class_identifier]=dimmi_forum_reply
+Match[section_identifier]=sensor
+
 
 */ ?>
