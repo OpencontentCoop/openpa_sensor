40e1e38f6fd235f403133f00498c154e7a3272c6
Luca Realdi <luca.realdi@opencontent.it>
2015-03-03 12:57:31 +0100

    edit post

---
+++ modules/sensor/edit.php
@@ -0,0 +1,14 @@
+<?php
+/** @var eZModule $module */
+$module = $Params['Module'];
+$http = eZHTTPTool::instance();
+$id = $Params['ID'];
+$object = eZContentObject::fetch( $id );
+if ( $object instanceof eZContentObject )
+{
+    eZSys::addAccessPath( array( 'layout', 'set', 'sensor_add' ), 'layout', false );
+    $module->redirectTo( 'content/edit/' . $object->attribute( 'id' ) . '/' . $object->attribute( 'current_language' ) );
+    return;
+}
+else
+    return $module->handleError( eZError::KERNEL_NOT_AVAILABLE, 'kernel' );
