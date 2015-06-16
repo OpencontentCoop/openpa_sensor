86d2dee13a4270ac04870710a483aca9a5e65fae
Luca Realdi <luca.realdi@opencontent.it>
2015-06-16 16:34:27 +0200

    revert previous commit

--- collaborationhandlers/sensor/helpers/fetcher.php
+++ collaborationhandlers/sensor/helpers/fetcher.php
@@ -286,7 +286,6 @@ class SensorPostFetcher
             if ( !empty( $categoryFilter ) )
             {
                 $search =$solr->search( '', array(
-                        'Limitation' => array(),
                         'Filter' => $categoryFilter,
                         'SearchContentClassID' => array( ObjectHandlerServiceControlSensor::postContentClass()->attribute( 'id' ) ),
                         'SearchSubTreeArray' => array( 1 ) ) );
@@ -321,7 +320,6 @@ class SensorPostFetcher
             {
                 $solr = new eZSolr();
                 $search = $solr->search( $creatorId, array(
-                        'Limitation' => array(),
                         'SearchContentClassID' => array( 'user', 'sensor_operator' ),
                         'SearchSubTreeArray' => array( 1 ) ) );
                 if ( $search['SearchCount'] > 0 )
