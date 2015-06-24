26f1659592c08820790c858184f46ac910a7d117
Luca Realdi <luca.realdi@opencontent.it>
2015-06-24 18:22:57 +0200

    bugfix and context pagedata

--- settings/social_design.ini.append.php
+++ settings/social_design.ini.append.php
@@ -1,7 +1,12 @@
 <?php /* #?ini charset="utf-8"?
 
 [GeneralSettings]
-PageDataHandler=ObjectHandlerServiceControlSensor
+PageDataHandler[sensor]=ObjectHandlerServiceControlSensor
+AvailableContext[]=sensor
 
+
+[ContextSettings_sensor]
+Identifier=sensor
+MatchSiteaccessName=_sensor
 
 */ ?>
