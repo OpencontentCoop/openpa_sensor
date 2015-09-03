6b559764b58e1dded8d88210d622070ff312e92b
OpenContent <luca.realdi@opencontent.it>
2015-09-03 16:02:11 +0200

    Add all notification rules to operators script

---
+++ bin/php/add_all_notification_rules_to_operators.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'autoload.php';
+
+$script = eZScript::instance( array( 'description' => ( "OpenPA Sensor Add all notification rules to operators\n\n" ),
+                                     'use-session' => false,
+                                     'use-modules' => true,
+                                     'use-extensions' => true ) );
+
+$script->startup();
+
+$options = $script->getOptions();
+$script->initialize();
+$script->setUseDebugAccumulators( true );
+
+$cli = eZCLI::instance();
+
+OpenPALog::setOutputLevel( OpenPALog::ALL );
+/** @var eZUser $user */
+$user = eZUser::fetchByName( 'admin' );
+eZUser::setCurrentlyLoggedInUser( $user , $user->attribute( 'contentobject_id' ) );
+
+try
+{
+    $operators = ObjectHandlerServiceControlSensor::operators();
+    foreach( $operators as $operator )
+    {
+        SensorNotificationHelper::instance()->storeNotificationRules( $operator->attribute( 'contentobject_id' ) );
+    }
+
+    $script->shutdown();
+}
+catch( Exception $e )
+{
+    $errCode = $e->getCode();
+    $errCode = $errCode != 0 ? $errCode : 1; // If an error has occured, script must terminate with a status other than 0
+    $script->shutdown( $errCode, $e->getMessage() );
+}
