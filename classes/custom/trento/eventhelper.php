91cc52c4b50334b94fb4cf1c70e184eb2f14c07a
Luca Realdi <lr@opencontent.it>
2015-11-17 15:41:40 +0100

    fix TrentoEventHelper inheritance

--- classes/custom/trento/eventhelper.php
+++ classes/custom/trento/eventhelper.php
@@ -1,56 +1,10 @@
 <?php
 
-class TrentoSensorPostEventHelper implements SensorPostEventHelperInterface
+class TrentoSensorPostEventHelper extends SensorPostEventHelper
 {
-    /**
-     * @var SensorPost
-     */
-    protected $post;
-
-    /**
-     * @var SensorNotificationHelper
-     */
-    protected $notificationHelper;
-
-    public $availableEvents = array(
-        'on_create',
-        'on_update',
-        'on_assign',
-        'on_fix',
-        'on_force_fix',
-        'on_close',
-        'on_make_private',
-        'on_make_public',
-        'on_moderate',
-        'on_add_observer',
-        'on_add_category',
-        'on_add_area',
-        'on_set_expiry',
-        'on_add_comment',
-        'on_reopen',
-        'on_add_message',
-        'on_add_response',
-        'on_edit_comment',
-        'on_edit_message',
-        'on_restore',
-        'on_remove'
-    );
-
-    public function __construct( SensorPost $post )
-    {
-        $this->post = $post;
-        $this->notificationHelper = SensorNotificationHelper::instance( $this->post );
-    }
-
-    public function createEvent( $eventName )
+    public function createEvent( $eventName, $eventDetails = array() )
     {        
-        foreach( $this->notificationHelper->postNotificationTypes() as $type )
-        {
-            if ( $type['identifier'] ==  $eventName )
-            {
-                $this->post->getCollaborationItem()->createNotificationEvent( $eventName );
-            }
-        }
+        parent::createEvent( $eventName, $eventDetails );
         
         if ( $eventName == 'on_create' || $eventName == 'on_update' )
         {
@@ -65,9 +19,4 @@ class TrentoSensorPostEventHelper implements SensorPostEventHelperInterface
             }
         }
     }
-
-    public function handleEvent( eZNotificationEvent $event, array &$parameters )
-    {
-        return $this->notificationHelper->handleEvent( $event, $parameters );
-    }        
 }
